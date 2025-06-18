2796e04cb85eae7d4a0317b0612cdbac80fc3c54
mdounzaidi <mdounzaidi@gmail.com>
2025-06-18 19:00:32 +0530

    test

--- src/main/java/com/mdounzaidi/retro/url/shortner/controller/HealthCheck.java
+++ src/main/java/com/mdounzaidi/retro/url/shortner/controller/HealthCheck.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class HealthCheck {
 
-    @GetMapping("/healthcheck")
+    @GetMapping("/healthcheck404")
     String checkhealth(){
         return "ok";
     }
